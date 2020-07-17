25d15e3c5524d309df3d469237abfbc7669c9316
narges <nargestv@gmail.com>
2020-07-17 23:44:57 +0430

    add perm

--- app/Http/Controllers/Type/TypeController.php
+++ app/Http/Controllers/Type/TypeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Type;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Type;
+use Auth;
+use JWTAuth;
+use App\Models\User;
 
 class TypeController extends Controller
 {
@@ -15,11 +18,29 @@ class TypeController extends Controller
 
     public function adminList(Request $request)
     {
+       // $user = User::find(Auth::user()->id);
+        $user = User::find(1);
+        $userPermissions = $user->getPermissionList();
+        if($userPermissions){
+            foreach ($userPermissions as $userPermission){
+                if ($request->has('for') && $request->for == 'add'){
+                    if (strpos($userPermission['on'],'insert') !== false){
+                        $list[] =str_replace("insert_","",$userPermission['on']);
+                    }
+                }
+                if ($request->has('for') && $request->for == 'edit') {
+                    if (strpos($userPermission['on'], 'edit') !== false) {
+                        $list[] = str_replace("edit_", "", $userPermission['on']);
+                    }
+                }
+            }
+        }
         if ($request->has('model') && $request->has('use_for')) {
             $types = Type::where('typeable_type', 'App\Models\\' . ucfirst($request->model))
                 ->whereHas('Models', function ($query) use ($request) {
                     $query->where('model', '=', 'App\Models\\' . ucfirst($request->use_for));
                 })
+                ->whereIn('slug', $list)
                 ->latest()
                 ->get();
         } elseif ($request->has('model')) {
